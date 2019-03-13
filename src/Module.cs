6ad2e40d018b9493897e2b81137da7f6456f0d13
restlessmedia <phillycooper@hotmail.com>
2019-03-13 20:13:23 +0000

    Registered dataprovider

--- src/Module.cs
+++ src/Module.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using restlessmedia.Module.File.Configuration;
+using restlessmedia.Module.File.Data;
 
 namespace restlessmedia.Module.File
 {
@@ -9,6 +10,7 @@ namespace restlessmedia.Module.File
     {
       containerBuilder.RegisterType<FileSystemStorageProvider>().As<IDiskStorageProvider>().SingleInstance();
       containerBuilder.RegisterType<FileService>().As<IFileService>().SingleInstance();
+      containerBuilder.RegisterType<FileDataProvider>().As<IFileDataProvider>().SingleInstance();
       containerBuilder.RegisterSettings<IAzureSettings>("restlessmedia/azure", required: true);
       containerBuilder.RegisterSettings<IFileSettings>("restlessmedia/file", required: true);
     }
