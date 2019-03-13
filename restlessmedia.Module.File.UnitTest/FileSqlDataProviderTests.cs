e19b5bbcff6fb2950d382def7387b9b486bece0b
restlessmedia <phillycooper@hotmail.com>
2019-03-13 18:03:48 +0000

    Added unit test project

---
+++ restlessmedia.Module.File.UnitTest/FileSqlDataProviderTests.cs
@@ -0,0 +1,68 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using restlessmedia.Module.Data;
+using restlessmedia.Module.File.Data;
+using System;
+
+namespace restlessmedia.Module.File.UnitTest
+{
+  [TestClass]
+  public class FileSqlDataProviderTests
+  {
+    public FileSqlDataProviderTests()
+    {
+      _dataContext = A.Fake<IDataContext>();
+      _dataProvider = new FileSqlDataProvider(_dataContext);
+
+      A.CallTo(() => _dataContext.ConnectionFactory).Returns(new IntegrationTestConnetionFactory());
+      A.CallTo(() => _dataContext.LicenseSettings.LicenseKey).Returns(new Guid(licenseKey));
+    }
+
+    //[TestMethod]
+    public void Save_creates_record()
+    {
+      FileEntity file = new FileEntity
+      {
+        FileName = "test2.jpg",
+        SystemFileName = "test2.jpg"
+      };
+
+      //  A.CallTo(() => _dataContext.ConnectionFactory.CreateConnection()).Invokes<SqlConnection>(() =>
+      //{
+      //  return new SqlConnection(connectionString);
+      //});
+
+
+      //(new SqlConnection(connectionString));
+
+
+      FileEntity fileEntity = _dataProvider.Read(54552); 
+
+      _dataProvider.Save(EntityType.Property, 21643, file);
+
+      fileEntity = _dataProvider.Read(file.FileId.Value);
+
+      _dataProvider.Delete(fileEntity.FileId.Value);
+    }
+
+    //[TestMethod]
+    public void list_files()
+    {
+      var list = _dataProvider.List(EntityType.Property, 21643);
+    }
+
+    //[TestMethod]
+    public void read_file()
+    {
+      var file = _dataProvider.Read(35866);
+    }
+
+    private readonly FileSqlDataProvider _dataProvider;
+
+    private readonly IDataContext _dataContext;
+
+    private const string connectionString = @"Server=.\SQLEXPRESS;database=restlessmedia.UnitTest;Trusted_Connection=Yes;APP=PC";
+
+    private const string licenseKey = "BF812A79-9AE3-48F7-8210-FDA1DF411318";
+  }
+}
---
+++ restlessmedia.Module.File.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("restlessmedia.Module.File.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("restlessmedia.Module.File.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7c39901-d97e-4a9f-90c3-b965f7397adf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
