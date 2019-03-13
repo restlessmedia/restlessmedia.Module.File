f77dfeb77804f0792db566391d9f977f2f832d80
restlessmedia <phillycooper@hotmail.com>
2019-03-13 18:51:05 +0000

    Tests

---
+++ restlessmedia.Module.File.UnitTest/MimeTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace restlessmedia.Module.File.UnitTest
+{
+  [TestClass]
+  public class MimeTests
+  {
+    [TestMethod]
+    public void TestJpgExtensionWithoutPeriodReturnsJpgMimeType()
+    {
+      Assert.AreEqual(MimeExtensions.GetMimeType("jpg"), "image/jpeg");
+    }
+
+    [TestMethod]
+    public void TestJpgExtensionWithPeriodReturnsJpgMimeType()
+    {
+      Assert.AreEqual(MimeExtensions.GetMimeType(".jpg"), "image/jpeg");
+    }
+
+    [TestMethod]
+    public void TestNonExistentExtensionReturnsNull()
+    {
+      MimeExtensions.GetMimeType("xxx").ShouldBeNull();
+    }
+  }
+}
