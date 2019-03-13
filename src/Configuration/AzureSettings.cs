c1eb976ded9a3a5d55ef461dae584d91efe00750
restlessmedia <phillycooper@hotmail.com>
2019-03-13 14:49:00 +0000

    Added AzureSettings

---
+++ src/Configuration/AzureSettings.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using System.Configuration;
+
+namespace restlessmedia.Module.File.Configuration
+{
+  public class AzureSettings : SerializableConfigurationSection, IAzureSettings
+  {
+    public IAzureStorageSettings Storage
+    {
+      get
+      {
+        return StorageElement;
+      }
+    }
+
+    [ConfigurationProperty(_storageProperty)]
+    private AzureStorageSettings StorageElement
+    {
+      get
+      {
+        return (AzureStorageSettings)this[_storageProperty];
+      }
+    }
+
+    private const string _storageProperty = "storage";
+  }
+}
---
+++ src/Configuration/AzureStorageSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace restlessmedia.Module.File.Configuration
+{
+  public class AzureStorageSettings : ConfigurationElement, IAzureStorageSettings
+  {
+    [ConfigurationProperty(_connectionStringProperty, IsRequired = true)]
+    public string ConnectionString
+    {
+      get
+      {
+        return (string)this[_connectionStringProperty];
+      }
+      set
+      {
+        this[_connectionStringProperty] = value;
+      }
+    }
+
+    private const string _connectionStringProperty = "connectionString";
+  }
+}
